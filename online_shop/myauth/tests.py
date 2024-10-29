4839a97520ca882600f4e60878bb8ab3a3b2a873
Станислав Рубцов <frex@mail.ru>
2024-10-29 12:57:25 +0300

    add UserViewTestCase

--- online_shop/myauth/tests.py
+++ online_shop/myauth/tests.py
@@ -1,3 +1,35 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+
+class UserViewTestCase(TestCase):
+
+    def test_registration_user_bad_password(self):
+        """
+        Тестирование на надежность пароля
+        """
+        user = {
+            "username": "TestUser",
+            "password": "123456"
+        }
+        response = self.client.post(reverse("api:sign_up"), user)
+        received_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(received_data["password"][0], "Invalid password")
+
+    def test_registration_user(self):
+        """
+        Тестирование на регистрацию пользователя
+        """
+        user = {
+            "username": "TestUser",
+            "password": "1qaz!QAZ"
+        }
+        response = self.client.post(reverse("api:sign_up"), user)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(
+            User.objects.filter(username="TestUser").exists()
+        )
