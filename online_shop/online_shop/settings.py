8b2c87ea2d581a3472298236b7968ec176c760dc
Станислав Рубцов <frex@mail.ru>
2024-10-30 19:39:57 +0300

    add postgresql

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -93,10 +93,15 @@ INTERNAL_IPS = [
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
