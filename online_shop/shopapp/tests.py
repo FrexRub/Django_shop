bfb0de0654e15bb14f8ff700bfea3dde0dd40845
SRubtsov <frexrub@gmail.com>
2024-11-14 14:08:13 +0300

    create shopapp.tests

--- online_shop/shopapp/tests.py
+++ online_shop/shopapp/tests.py
@@ -1,3 +1,21 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+from .models import Product, Tag
+
+
+class TagsTestCase(TestCase):
+    fixtures = ["data.json"]
+
+    def test_get_tags(self):
+        """
+        Тестирование выгрузки тэгов
+        """
+        data = {"category": 1}
+        response = self.client.post(reverse("api:tags"), data)
+        received_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(received_data["message"], "ok")
