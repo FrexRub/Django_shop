3033fb05f35733382d8d735ed049f8987229c865
Станислав Рубцов <frex@mail.ru>
2024-11-26 21:51:42 +0300

    add BasketTestCase

--- online_shop/basket/tests.py
+++ online_shop/basket/tests.py
@@ -1,3 +1,16 @@
+import json
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from shopapp.models import Product
+
+
+class BasketTestCase(TestCase):
+    fixtures = ["data.json"]
+
+    def test_add_product(self):
+        data = {"id": 1, "count": 1}
+        response = self.client.post(reverse("api:basket"), data)
+        received_data = json.loads(response.content)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(received_data[0]["price"], "67399.00")
