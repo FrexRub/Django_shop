22589ee032172a66f29a7f9a9c3459b4638eaa18
Станислав Рубцов <frex@mail.ru>
2024-11-26 22:14:36 +0300

    add BasketTestCase (add test_delete_product)

--- online_shop/basket/tests.py
+++ online_shop/basket/tests.py
@@ -26,3 +26,21 @@ class BasketTestCase(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(received_data[0]["price"], "67399.00")
         self.assertEqual(count_product_before - count_product_after, count_product)
+
+    def test_delete_product(self):
+        id_product = 1
+        count_product = 2
+        data = {"id": id_product, "count": count_product}
+        response = self.client.post(reverse("api:basket"), data)
+        received_data = json.loads(response.content)
+
+        count_add = received_data[0]["count"]
+
+        data = {"id": id_product, "count": count_product - 1}
+        response = self.client.delete(reverse("api:basket"), data)
+        # received_data = json.loads(response.content)
+        print(response)
+        # count_after_delete = received_data[0]["count"]
+
+        self.assertEqual(response.status_code, 200)
+        # self.assertEqual(count_add - count_after_delete, count_product - 1)
