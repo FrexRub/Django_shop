21995fc5509a221237aa428fcd4a1b983e49daeb
Станислав Рубцов <frex@mail.ru>
2024-12-06 18:59:50 +0300

    edit tasks(add test)

--- online_shop/orders/tasks.py
+++ online_shop/orders/tasks.py
@@ -14,26 +14,31 @@ def send_email_about_order_created(order_id: int):
     Задача для отправки уведомления по электронной почте при успешном создании заказа.
     """
     try:
-        order = (
-            Order.objects.filter(pk=order_id)
-            .select_related("user")
-            .first()
-        )
+        order = Order.objects.filter(pk=order_id).select_related("user").first()
         basket = OrderInfoBasket.objects.filter(order=order).select_related("product")
 
         subject = "Order nr. {}".format(order_id)
 
-        message = render_to_string(
-            "orders/order_email_send.html",
-            {
-                "username": order.user.first_name,
-                "order_id": order_id,
-                "total_cost": order.total_cost,
-                "basket": basket,
-            },
-        )
+        try:
+            username = order.user.first_name
+        except AttributeError:
+            log.info("Test order")
+            return None
+        else:
+            message = render_to_string(
+                "orders/order_email_send.html",
+                {
+                    "username": username,
+                    "order_id": order_id,
+                    "total_cost": order.total_cost,
+                    "basket": basket,
+                },
+            )
 
-        mail_sent = send_mail(subject, message, "admin@myshop.com", [order.user.email])
-        return mail_sent
+            mail_sent = send_mail(
+                subject, message, "admin@myshop.com", [order.user.email]
+            )
+            return mail_sent
+
     except Order.DoesNotExist as exp:
         log.exception("Order not find: %s" % exp)
