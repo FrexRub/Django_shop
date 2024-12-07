5f89966f5883ed591850310597064b912205e3f9
Станислав Рубцов <frex@mail.ru>
2024-12-07 22:13:12 +0300

    edit settings(add host names for REDIS and POSTGRES)

--- online_shop/online_shop/settings.py
+++ online_shop/online_shop/settings.py
@@ -18,6 +18,9 @@ env = environ.Env(
     DJANGO_DEBUG=(bool, False),
     DJANGO_LOGLEVEL=(str, "info"),
     DJANGO_ALLOWED_HOSTS=(str, ""),
+    POSTGRES_HOST=(str, "localhost"),
+    REDIS_HOST_CACHES=(str, "redis://127.0.0.1:6379/1"),
+    REDIS_HOST_CELERY=(str, "redis://127.0.0.1:6379/0"),
 )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +115,8 @@ DATABASES = {
         "NAME": env("POSTGRES_DB"),
         "USER": env("POSTGRES_USER"),
         "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": "localhost",
+        "HOST": env("POSTGRES_HOST"),
+        # "HOST": "localhost",
         "PORT": env("POSTGRES_PORT"),
     }
 }
@@ -124,14 +128,17 @@ SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        # "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": env("REDIS_HOST_CACHES"),
         "OPTIONS": {"CLIENT_CLASS": "django_redis.client.DefaultClient"},
         "KEY_PREFIX": "cache_shop",
     }
 }
 
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+# CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+# CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = env("REDIS_HOST_CACHES")
+CELERY_RESULT_BACKEND = env("REDIS_HOST_CACHES")
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_ACCEPT_CONTENT = ["application/json"]
